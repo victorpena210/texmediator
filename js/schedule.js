0bf53924bcb15622246a54f1331e428c1bf221d2
Victor Pena <victor.javier.pena210@gmail.com>
2026-08-19 18:21:27 -0500

    testing calendar

--- js/schedule.js
+++ js/schedule.js
@@ -88,7 +88,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     window.matchMedia(
                         "(max-width: 700px)"
                     ).matches
-                        ? 350
+                        ? 400
                         : 400;
 
                 window.scrollTo({
